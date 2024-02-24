a4d61c67c17197ef6d557d265b60fde2c2c4e4b5
unstoppableTANMAY <iamunstoppableguy@gmail.com>
2024-02-25 03:07:32 +0530

    added all now it should be alright

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -137,7 +137,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
